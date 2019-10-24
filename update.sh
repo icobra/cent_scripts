e0437cfbc97916f4692384dde69aeb2ae0ffba93
Sasha Good <eiroglif@mail.ru>
2019-10-24 12:08:46 +0000

    Add auto -y answer

--- update.sh
+++ update.sh
@@ -7,11 +7,11 @@ yum -y upgrade
 # kernel upgrade to 5.1 >
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-ml
+yum -y install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+yum -y --enablerepo=elrepo-kernel install kernel-ml
 
 sed -i -e 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/g' /etc/default/grub
 
-grub2-mkonfig -o /boot/grub2/grub.cfg
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
 reboot
